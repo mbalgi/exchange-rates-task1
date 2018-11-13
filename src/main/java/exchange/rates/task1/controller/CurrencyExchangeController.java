6484ebffb2885bb2aa9da9b9630abbffe3b06c9b
manoj balgi <manojmbalgi@gmail.com>
2018-11-13 19:39:00 +0400

    called the currency external service to get the exchange rate. working condition

--- src/main/java/exchange/rates/task1/controller/CurrencyExchangeController.java
+++ src/main/java/exchange/rates/task1/controller/CurrencyExchangeController.java
@@ -1,16 +1,35 @@
 package exchange.rates.task1.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import exchange.rates.task1.dto.response.CurrencyExchangeResponse;
+import io.micronaut.http.HttpRequest;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.client.RxHttpClient;
+import io.micronaut.http.client.annotation.Client;
+
+import javax.inject.Inject;
+import java.io.IOException;
+import java.util.Map;
 
 
-@Controller("/api/v1")
+@Controller("/api")
 public class CurrencyExchangeController {
 
-    @Get("/{from_currency}/{to_currency}/{from_amount}")
-    public CurrencyExchangeResponse getCurrencyExchangeRate(String from_currency, String to_currency, double from_amount) {
+    @Client("https://free.currencyconverterapi.com/api/v6")
+    @Inject
+    RxHttpClient currencyExchangeExternalClient;
 
-        return new CurrencyExchangeResponse("AED", "INR", 1.0, 19.78, 19.79);
+    @Get("/v1/{from_currency}/{to_currency}/{from_amount}")
+    public CurrencyExchangeResponse getCurrencyExchangeRate(String from_currency, String to_currency, double from_amount) throws IOException {
+
+        String fromToCurrency = from_currency +"_" + to_currency;
+        String body = currencyExchangeExternalClient.toBlocking().retrieve(HttpRequest.GET("/convert?q=" + fromToCurrency + "&compact=y"));
+
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String,Object> responseMap = (Map<String, Object>) mapper.readValue(body, Map.class).get(fromToCurrency);
+        double exchangeRate = (double) responseMap.get("val");
+
+        return new CurrencyExchangeResponse(from_currency, to_currency, from_amount, from_amount*exchangeRate, exchangeRate);
     }
 }
